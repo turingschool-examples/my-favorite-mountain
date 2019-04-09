a955bf1bbb75a7bfe560248e94935b15d8ed4f34
Robbie Jaeger <jaeger.rob@gmail.com>
2019-04-09 10:19:29 -0600

    Add title in header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <h1>My Favorite Mountain</h1>
+          <p>Where You Can Favorite Your Favorite Mountain</p>
         </header>
+
+        
       </div>
     );
   }
